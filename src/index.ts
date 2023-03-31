a31aec12412c8b6204c59113058474126ec558c2
Greg <25317592+GregEisenberg@users.noreply.github.com>
2023-03-30 21:51:02 -0700

    testing line lengths ;-) really

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Dealer } from "./dealer";
 export { standardDeck, StandardPlayingCard } from "./decks/standard";
 export { unoDeck } from "./decks/uno";
+1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
